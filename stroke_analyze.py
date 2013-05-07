9dad05fac3e424e8f8da43a13ad827b52619d93a
Adam MacBeth <amacbeth@gmail.com>
2013-05-07 12:22:54 -0700

    Refactor state management into a class. Count missing up/down.

--- stroke_analyze.py
+++ stroke_analyze.py
@@ -20,18 +20,8 @@ PEN_TIP2 = 1
 log_events = 1
 log_deltas = 1
 
-deltas = [ [], [] ]
-
-state = {}
-state['touches'] = {}
-state['stroke_count'] = 0
-
 class PenEvent:
     def __init__(self, data=None):
-        if data is None:
-            self.state = PEN_UP
-            return
-
         values = data.split(',')
         self.state = int(values[0])
         self.tip = int(values[1])
@@ -84,132 +74,158 @@ class TouchEvent:
         else:
             phase = "UNKNOWN"
             
-        return "touch id=%d phase=%s time=%f" % (self.id, phase, self.timestamp)
+        return "touch phase=%s id=%d time=%f" % (phase, self.id, self.timestamp)
 
-def log_delta():
-    pen = state['last_pen']
-    touch = state['last_touch']
-    assert((pen.state == PEN_UP and touch.phase == TOUCH_ENDED)
-           or (pen.state == PEN_DOWN and touch.phase == TOUCH_BEGAN))
-    
-    delta = pen.timestamp - touch.timestamp
-    deltas[pen.state].append(delta)
+class Analyzer:
+    def __init__(self):
+        self.deltas = [ [], [] ]
+
+        self.stroke_count = 0
+        self.missing_up = 0
+        self.missing_down = 0
+        self.last_pen = None
+        self.last_touch = None
         
-    if log_deltas:
-        print "pen_delta (%s, %s) %f" % (pen.tip_str(), pen.state_str(), delta)
-
-def plot(x):
-    import matplotlib.pyplot as plt
-    import numpy as np
-
-    hist, bins = np.histogram(x,bins = 50)
-    width = 0.7*(bins[1]-bins[0])
-    center = (bins[:-1]+bins[1:])/2
-    plt.bar(center, hist, align = 'center', width = width)
-    plt.show()
-
-def process_touch(touch):
-    if touch.phase != TOUCH_MOVED:
-        state['last_touch'] = touch
-    
-    if touch.phase == TOUCH_BEGAN:
-        if log_events:
-            print 'touch began: %s' % (touch)
-
-        state['stroke_count'] += 1
-        state['touches'][touch.id] = touch
-
-        state['last_pen'] 
-        if (state['last_pen'].state == PEN_DOWN):
-            log_delta()
-    elif touch.phase == TOUCH_ENDED or touch.phase == TOUCH_CANCELLED:
-        ended = touch
-        began = state['touches'][touch.id]
-
-        if log_events:
-            print 'touch began: %s, duration=%f' % (touch, ended.timestamp - began.timestamp)
-
-        if (state['last_pen'].state == PEN_UP):
-            log_delta()
-
-        del state['touches'][touch.id]
-#    print state['touches']
-
-def process_pen(pen):
-    if log_events:
-        print pen
-    
-    if (pen.state == state['last_pen'].state):
-        print "WARNING: duplicate pen state received"
-
-    state['last_pen'] = pen
-
-    if not state['last_touch']:
-        return
-
-    if (pen.state == PEN_UP and state['last_touch'].phase == TOUCH_ENDED):
-        log_delta()
-    elif (pen.state == PEN_DOWN and state['last_touch'].phase == TOUCH_BEGAN):
-        log_delta()
-
-def parse_file(filename):
-    f = open(filename)
-    for line in f:
-        line_data = line.split('=')
-        event_type = line_data[0]
-        event_data = line_data[1]
-
-        if event_type == 'touch':
-            touch = TouchEvent(event_data)
-            process_touch(touch)
-        elif event_type == 'pen':
-            pen = PenEvent(event_data)
-            process_pen(pen)
-
-    if len(state['touches']) != 0:
-        print "WARNING: not all touches ended"
-
-    if state['last_pen'] != PEN_UP:
-        print "WARNING: pen did not end UP"
-
-    print_summary()
-
-def print_summary():
-    print "\nSUMMARY"
-    print "========================="
-    print "stroke count = %d" % (state['stroke_count'])
-    
-    for i in [ PEN_DOWN, PEN_UP ]:
-        print
-        if i == PEN_UP:
-            print "PEN_UP stats:"
-        else:
-            print "PEN_DOWN stats:"
+    def log_delta(self):
+        assert((self.last_pen.state == PEN_UP and self.last_touch.phase == TOUCH_ENDED)
+               or (self.last_pen.state == PEN_DOWN and self.last_touch.phase == TOUCH_BEGAN))
+        
+        delta = self.last_pen.timestamp - self.last_touch.timestamp
+        self.deltas[self.last_pen.state].append(delta)
             
-        print "delta min = %f" % (numpy.min(deltas[i]))
-        print "delta max = %f" % (numpy.max(deltas[i]))
-        count_over = len([x for x in deltas[i] if x > 0])
-        count_under = len(deltas[i]) - count_over
-        
-        print "delta %% under 0 = %f (%d/%d)" % (float(count_under) / len(deltas[i]) * 100, count_under, len(deltas[i]))
-        print "delta %% over 0 = %f (%d/%d)" % (float(count_over) / len(deltas[i]) * 100, count_over, len(deltas[i]))
-        print "delta average = %f" % (numpy.average(deltas[i]))
-        print "delta stddev = %f" % (numpy.std(deltas[i]))
-        print "delta var = %f" % (numpy.var(deltas[i]))
+        if log_deltas:
+            print "pen_delta (%s, %s) %f" % (self.last_pen.tip_str(), self.last_pen.state_str(), delta)
 
-        plot(deltas[i])
+    def touch_began(self, touch):
+        if (self.last_touch is None and self.last_pen is None):
+            return
+
+        if (self.last_pen is None):
+            print 'missing up/down'
+            self.missing_down += 1
+            self.missing_up += 1
+            return
+        
+        if (self.last_pen.state == PEN_DOWN
+            and (self.last_touch is None
+                 or self.last_pen.timestamp > self.last_touch.timestamp)):
+            # pen down before touch began
+            self.last_touch = touch
+            self.log_delta()
+        else:
+            self.last_pen = None
+
+    def touch_ended(self, touch):
+        self.stroke_count += 1
+            
+        if (self.last_pen is not None):
+            if (self.last_pen.state == PEN_UP):
+                self.last_touch = touch
+                self.log_delta()
+
+    def process_touch(self, touch):
+        if touch.phase == TOUCH_MOVED:
+            return
+
+        if log_events:
+            print touch
+
+        if touch.phase == TOUCH_BEGAN:
+            self.touch_began(touch)
+        elif touch.phase == TOUCH_ENDED or touch.phase == TOUCH_CANCELLED:
+            self.touch_ended(touch)
+            
+        self.last_touch = touch
+
+    def process_pen(self, pen):
+        if log_events:
+            print pen
+    
+        if (self.last_pen is not None and pen.state == self.last_pen.state):
+            if pen.state == PEN_UP:
+                self.missing_down += 1
+            elif pen.state == PEN_DOWN:
+                self.missing_up += 1
+
+        self.last_pen = pen
+
+        if self.last_touch is None:
+            return
+
+        if (pen.state == PEN_UP and self.last_touch.phase == TOUCH_ENDED):
+            self.log_delta()
+        elif (pen.state == PEN_DOWN and self.last_touch.phase == TOUCH_BEGAN):
+            self.log_delta()
+
+    def parse_file(self, filename):
+        f = open(filename)
+        for line in f:
+            line_data = line.split('=')
+            event_type = line_data[0]
+            event_data = line_data[1]
+
+            if event_type == 'touch':
+                touch = TouchEvent(event_data)
+                self.process_touch(touch)
+            elif event_type == 'pen':
+                pen = PenEvent(event_data)
+                self.process_pen(pen)
+
+        if self.last_pen.state != PEN_UP:
+            print "WARNING: pen did not end UP"
+
+        self.print_summary()
+        
+    def plot(self, x):
+        import matplotlib.pyplot as plt
+        import numpy as np
+
+        hist, bins = np.histogram(x,bins = 50)
+        width = 0.7*(bins[1]-bins[0])
+        center = (bins[:-1]+bins[1:])/2
+        plt.bar(center, hist, align = 'center', width = width)
+        plt.show()
+        
+    def print_summary(self):
+        print "\nSUMMARY"
+        print "========================="
+        print "stroke count = %d" % (self.stroke_count)
+        print "missing down = %d" % (self.missing_down)
+        print "missing up = %d" % (self.missing_up)
+
+        print "percent missing = %f" % (float(self.missing_down) / self.stroke_count * 100)
+    
+        for i in [ PEN_DOWN, PEN_UP ]:
+            print
+            if i == PEN_UP:
+                print "PEN_UP stats:"
+            else:
+                print "PEN_DOWN stats:"
+
+            if len(self.deltas[i]) != 0:
+                print "delta min = %f" % (numpy.min(self.deltas[i]))
+                print "delta max = %f" % (numpy.max(self.deltas[i]))
+        
+                count_over = len([x for x in self.deltas[i] if x > 0])
+                count_under = len(self.deltas[i]) - count_over
+        
+                print "delta %% under 0 = %f (%d/%d)" % (float(count_under) / len(self.deltas[i]) * 100, count_under, len(self.deltas[i]))
+                print "delta %% over 0 = %f (%d/%d)" % (float(count_over) / len(self.deltas[i]) * 100, count_over, len(self.deltas[i]))
+                print "delta average = %f" % (numpy.average(self.deltas[i]))
+                print "delta stddev = %f" % (numpy.std(self.deltas[i]))
+                print "delta var = %f" % (numpy.var(self.deltas[i]))
+                
+                self.plot(self.deltas[i])
         
 def main():
-    state['last_pen'] = PenEvent()
-    state['last_touch'] = None
-    
     parser = argparse.ArgumentParser(description='Process pen and touch logs.')
     parser.add_argument('file',
                         help='the file to read data from')
 
     args = parser.parse_args()
 
-    parse_file(args.file)
+    analyzer = Analyzer()
+    analyzer.parse_file(args.file)
 
 if __name__ == "__main__":
     main()
