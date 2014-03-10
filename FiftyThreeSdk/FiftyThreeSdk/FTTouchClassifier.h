59b62c5fc374a46d43a83dfea2e2e90022b2de4d
Peter Sibley <peter@fiftythree.com>
2014-03-10 14:34:07 -0400

    Add FTTouchClassifier interface around fiftythree::sdk::TouchClassifier

---
+++ FiftyThreeSdk/FiftyThreeSdk/FTTouchClassifier.h
@@ -0,0 +1,59 @@
+//
+//  FTTouchClassifier.h
+//  FiftyThreeSdk
+//
+//  Copyright (c) 2014 FiftyThree, Inc. All rights reserved.
+//
+
+#pragma once
+
+#import <Foundation/Foundation.h>
+
+@class UITouch;
+
+typedef NS_ENUM(NSInteger, FTTouchClassification) {
+    // whenever the Pen isn't connected we return this default state.
+    FTTouchClassificationUnknownDisconnected,
+    // whenever we don't know what the touch is.
+    FTTouchClassificationUnknown,
+    // whenever we think the touch is a single finger. Note, this at
+    // the moment only is triggered if there's 1 touch active on the screen.
+    FTTouchClassificationFinger,
+    // whenever we think the touch is a palm touch
+    FTTouchClassificationPalm,
+    // whenever we think the touch correspoinds to the pen tip.
+    FTTouchClassificationTip1,
+    // whenever we think the touch corresoponds to the eraser.
+    FTTouchClassificationTip2,
+};
+
+@interface FTTouchClassificationInfo : NSObject
+@property (nonatomic) UITouch *touch;
+@property (nonatomic) FTTouchClassification oldValue;
+@property (nonatomic) FTTouchClassification newValue;
+@end
+
+@protocol FTTouchClassificationsChangedDelegate <NSObject>
+
+// touches is a NSSet of FTTouchClassificationInfo objects.
+// Touches may be reclassified after we've gotten more information about
+// the stylus.
+- (void)classificationsDidChangeForTouches:(NSSet *)touches;
+
+@end
+
+@interface FTTouchClassifier : NSObject
+
+// Register for change notification via this delegate.
+@property (nonatomic, weak)   id <FTTouchClassificationsChangedDelegate>   delegate;
+
+// Returns true if the touch is currently being tracked. Otherwise it returns false.
+- (BOOL)classification:(FTTouchClassification *)result forTouch:(UITouch *)touch;
+
+// Indiciates that a touch should no longer be considered a candidate for pen
+// classification. This case be useful if you're implementing custom GRs.
+// For example Paper's loupe control uses this to allow manipulation of the loupe and
+// smudging with one finger.
+- (void)removeTouchFromClassification:(UITouch *)touch;
+
+@end
---
+++ FiftyThreeSdk/FiftyThreeSdk/FTTouchClassifier.mm
@@ -0,0 +1,86 @@
+//
+//  FTTouchClassifier.mm
+//  FiftyThreeSdk
+//
+//  Copyright (c) 2014 FiftyThree, Inc. All rights reserved.
+//
+
+#import "Core/Memory.h"
+#import "Core/Touch/TouchTracker.h"
+#import "FiftyThreeSdk/TouchClassifier.h"
+#import "FTTouchClassifier.h"
+
+using namespace fiftythree::core;
+using namespace fiftythree::sdk;
+@implementation FTTouchClassifier
+
+- (BOOL)classification:(FTTouchClassification *)result forTouch:(UITouch *)touch
+{
+    Touch::Ptr ftTouch = static_pointer_cast<TouchTrackerObjC>(TouchTracker::Instance())->TouchForUITouch(touch);
+
+    if (ftTouch)
+    {
+        if (ftTouch->CurrentClassification() == TouchClassification::UntrackedTouch ||
+            ftTouch->CurrentClassification() == TouchClassification::RemovedFromClassification ||
+            ftTouch->CurrentClassification() == TouchClassification::Cancelled)
+
+        {
+            return NO;
+        }
+        else
+        {
+            switch (ftTouch->CurrentClassification()())
+            {
+                case TouchClassification::UnknownDisconnected:
+                {
+                    *result = FTTouchClassificationUnknownDisconnected;
+                }
+                break;
+                case TouchClassification::Unknown:
+                {
+                    *result = FTTouchClassificationUnknown;
+                }
+                break;
+                case TouchClassification::Finger:
+                {
+                    *result = FTTouchClassificationFinger;
+                }
+                break;
+                case TouchClassification::Palm:
+                {
+                    *result = FTTouchClassificationPalm;
+                }
+                break;
+                case TouchClassification::Pen:
+                {
+                    *result = FTTouchClassificationTip1;
+                }
+                break;
+                case TouchClassification::Eraser:
+                {
+                    *result = FTTouchClassificationTip2;
+                }
+                break;
+                default:
+                {
+                    DebugAssert(false);
+                }
+                break;
+            }
+            return YES;
+        }
+    }
+    return NO;
+}
+
+- (void)removeTouchFromClassification:(UITouch *)touch
+{
+    TouchClassifier::Ptr classifier = ActiveClassifier::Instance();
+    if (classifier)
+    {
+        Touch::Ptr ftTouch = static_pointer_cast<TouchTrackerObjC>(TouchTracker::Instance())->TouchForUITouch(touch);
+        classifier->RemoveTouchFromClassification(ftTouch);
+    }
+}
+
+@end
