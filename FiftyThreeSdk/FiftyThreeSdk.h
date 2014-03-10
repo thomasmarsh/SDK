70e7511803c6ccf562ed30cb236e4637bd90aad2
Peter Sibley <peter@fiftythree.com>
2014-03-10 14:42:15 -0400

    Top level include

---
+++ FiftyThreeSdk/FiftyThreeSdk.h
@@ -0,0 +1,14 @@
+//
+//  FiftyThreeSdk.h
+//  FiftyThreeSdk
+//
+//  Copyright (c) 2014 FiftyThree, Inc. All rights reserved.
+//
+
+#pragma once
+
+// Main entry point of the FiftyThree SDK for your application.
+#include "FTApplication.h"
+#include "FTLog.h"
+#include "FTSDKVersionInfo.h"
+#include "FTTouchClassifier.h"
