e9909ae47da8d60497954e7f6d2724528cacbb27
Peter Sibley <peter@fiftythree.com>
2014-06-05 14:56:51 -0400

    Suppress Eigen related register keyword warning

--- FiftyThreeSdk/FiftyThreeSdk/Classification/Eigen.h
+++ FiftyThreeSdk/FiftyThreeSdk/Classification/Eigen.h
@@ -11,9 +11,13 @@
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wconversion"
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-register"
+
 // Note this pulls in lot of extra numerics that only the classification project actually uses.
 #include <Eigen/Dense>
 #include <Eigen/Geometry>
 #include <Eigen/QR>
 
+#pragma clang diagnostic pop
 #pragma clang diagnostic pop
