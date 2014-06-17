1d3d8d3b99882cbe80aa1d6e747a61debad18244
Peter Sibley <peter@fiftythree.com>
2014-06-17 14:58:13 -0400

    FTXCallbackURL - provides x-callbacl-url parsing.

---
+++ FiftyThreeSdk/FiftyThreeSdk/FTXCallbackURL.h
@@ -0,0 +1,34 @@
+//
+//  FTXCallbackURL.h
+//  FiftyThreeSdk
+//
+//  Copyright (c) 2014 FiftyThree, Inc. All rights reserved.
+//
+
+#pragma once
+
+#import <Foundation/Foundation.h>
+
+// Internal helper for parsing x-callback-urls.
+@interface FTXCallbackURL : NSURL
+{
+}
+// Create a XCallbackUrl with named parameters. This sets up query parameters & url encodes the bits.
++ (FTXCallbackURL *)UrlWithScheme:(NSString *)scheme
+                             host:(NSString *)host
+                           action:(NSString *)action
+                           source:(NSString *)source
+                       successUrl:(NSURL *)success
+                         errorUrl:(NSURL *)error
+                        cancelUrl:(NSURL *)cancel;
+
+// Parse components etc.. out of an vanilla NSURL.
++ (FTXCallbackURL *)UrlWithNSURL:(NSURL *)other;
+
+@property (nonatomic, readonly) NSString *source;
+@property (nonatomic, readonly) NSURL *successUrl;
+@property (nonatomic, readonly) NSURL *errorUrl;
+@property (nonatomic, readonly) NSURL *cancelUrl;
+@property (nonatomic, readonly) NSString *action;
+
+@end
---
+++ FiftyThreeSdk/FiftyThreeSdk/FTXCallbackURL.m
@@ -0,0 +1,155 @@
+//
+//  FTXCallbackURL.m
+//  FiftyThreeSdk
+//
+//  Copyright (c) 2014 FiftyThree, Inc. All rights reserved.
+//
+
+#import "Core/NSString+urlEncoding.h"
+#import "FiftyThreeSdk/FTXCallbackURL.h"
+
+@interface FTXCallbackURL ()
+@property (nonatomic, readwrite) NSString *source;
+@property (nonatomic, readwrite) NSURL *successUrl;
+@property (nonatomic, readwrite) NSURL *errorUrl;
+@property (nonatomic, readwrite) NSURL *cancelUrl;
+@property (nonatomic, readwrite) NSString *action;
+@end
+
+NSString * const kFTXCallbackUrlSourceKey = @"x-source";
+NSString * const kFTXCallbackUrlErrorKey = @"x-error";
+NSString * const kFTXCallbackUrlCancelKey = @"x-cancel";
+NSString * const kFTXCallbackUrlSuccessKey = @"x-error";
+
+@implementation FTXCallbackURL
+// Create a XCallbackUrl with named parameters. This sets up query parameters & url encodes the bits.
++ (FTXCallbackURL *)UrlWithScheme:(NSString *)scheme
+                             host:(NSString *)host
+                           action:(NSString *)action
+                           source:(NSString *)source
+                       successUrl:(NSURL *)success
+                         errorUrl:(NSURL *)error
+                        cancelUrl:(NSURL *)cancel
+{
+    if (!scheme && !host && !action && !source)
+    {
+        return nil;
+    }
+
+    NSMutableString *baseUrl = [[NSString stringWithFormat:@"%@://%@/%@", scheme, host, action] mutableCopy];
+
+    if (success || error || cancel  || source)
+    {
+        //OK we have some query parameters start adding them
+        NSMutableArray *queryComponents = [@[] mutableCopy];
+
+        if (source)
+        {
+            NSMutableString *component = [@"" mutableCopy];
+            [component appendString:kFTXCallbackUrlSourceKey];
+            [component appendString:@"="];
+            [component appendString:[source urlEncodeUsingEncoding:NSUTF8StringEncoding]];
+            [queryComponents addObject:component];
+        }
+
+        if (success)
+        {
+            NSMutableString *component = [@"" mutableCopy];
+            [component appendString:kFTXCallbackUrlSuccessKey];
+            [component appendString:@"="];
+            [component appendString:[[success absoluteString] urlEncodeUsingEncoding:NSUTF8StringEncoding]];
+            [queryComponents addObject:component];
+        }
+
+        if (error)
+        {
+            NSMutableString *component = [@"" mutableCopy];
+            [component appendString:kFTXCallbackUrlErrorKey];
+            [component appendString:@"="];
+            [component appendString:[[success absoluteString] urlEncodeUsingEncoding:NSUTF8StringEncoding]];
+            [queryComponents addObject:component];
+
+        }
+
+        if (cancel)
+        {
+            NSMutableString *component = [@"" mutableCopy];
+            [component appendString:kFTXCallbackUrlCancelKey];
+            [component appendString:@"="];
+            [component appendString:[[success absoluteString] urlEncodeUsingEncoding:NSUTF8StringEncoding]];
+            [queryComponents addObject:component];
+
+        }
+        [baseUrl appendString:@"?"];
+        [baseUrl appendString:[@"&" join:queryComponents]];
+    }
+
+    FTXCallbackURL *url = [FTXCallbackURL URLWithString:baseUrl];
+
+    url.source = source;
+    url.action = action;
+    url.successUrl = success;
+    url.errorUrl = error;
+    url.cancelUrl = cancel;
+
+    return url;
+}
+
++(NSDictionary*)parseQueryString:(NSString*)query
+{
+    if ([query length] == 0)
+    {
+        return nil;
+    }
+
+    NSMutableDictionary* parameters = [NSMutableDictionary dictionary];
+
+    for (NSString* parameter in [query componentsSeparatedByString:@"&"])
+    {
+        NSRange range = [parameter rangeOfString:@"="];
+
+        if (range.location != NSNotFound)
+        {
+            [parameters setValue:[[parameter substringFromIndex:range.location+range.length] urlDecodeUsingEncoding:NSUTF8StringEncoding]
+                          forKey:[[parameter substringToIndex:range.location] urlDecodeUsingEncoding:NSUTF8StringEncoding]];
+        }
+        else
+        {
+            [parameters setValue:@"" forKey:[parameter urlDecodeUsingEncoding:NSUTF8StringEncoding]];
+        }
+    }
+    return parameters;
+}
+
++ (FTXCallbackURL *)UrlWithNSURL:(NSURL *)other
+{
+    FTXCallbackURL *url = [[FTXCallbackURL alloc] initWithString:[other absoluteString]];
+    NSDictionary *queryParameters = [FTXCallbackURL parseQueryString:other.query];
+
+    if ([queryParameters objectForKey:kFTXCallbackUrlSourceKey])
+    {
+        url.source = queryParameters[kFTXCallbackUrlSourceKey];
+    }
+
+    if ([queryParameters objectForKey:kFTXCallbackUrlSuccessKey])
+    {
+        url.successUrl = [NSURL URLWithString:queryParameters[kFTXCallbackUrlSuccessKey]];
+    }
+
+    if ([queryParameters objectForKey:kFTXCallbackUrlErrorKey])
+    {
+        url.errorUrl = [NSURL URLWithString:queryParameters[kFTXCallbackUrlErrorKey]];
+    }
+
+    if ([queryParameters objectForKey:kFTXCallbackUrlCancelKey])
+    {
+        url.cancelUrl = [NSURL URLWithString:queryParameters[kFTXCallbackUrlCancelKey]];
+    }
+
+    if (other.path)
+    {
+        url.action = url.path;
+    }
+}
+
+@end
