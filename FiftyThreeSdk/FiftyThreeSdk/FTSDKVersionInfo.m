0c6f546457d7bbadac12670acc8f527050feca62
Peter Sibley <peter@fiftythree.com>
2014-07-10 17:38:09 -0400

    Fix FTSDKVersionInfo

--- FiftyThreeSdk/FiftyThreeSdk/FTSDKVersionInfo.m
+++ FiftyThreeSdk/FiftyThreeSdk/FTSDKVersionInfo.m
@@ -2,31 +2,30 @@
 //  FTSDKVersionInfo.m
 //  FiftyThreeSdk
 //
-//  Created by Peter Sibley on 3/10/14.
-//  Copyright (c) 2014 FiftyThree. All rights reserved.
+//  Copyright (c) 2014 FiftyThree, Inc. All rights reserved.
 //
 
 #import "FTSDKVersionInfo.h"
 
 @implementation FTSDKVersionInfo
-- (NSString *) version
+- (NSString *)version
 {
     return [NSString stringWithFormat:@"%ld.%ld",(long)self.majorVersion,(long)self.minorVersion];
 }
-- (NSInteger) majorVersion
+- (NSInteger)majorVersion
+{
+    return 1;
+}
+- (NSInteger)minorVersion
 {
     return 0;
 }
-- (NSInteger) minorVersion
-{
-    return 5;
-}
-- (NSString *) commit
+- (NSString *)commit
 {
     return @"";
 }
 
-- (NSString *) timestamp
+- (NSString *)timestamp
 {
     return @"";
 }
