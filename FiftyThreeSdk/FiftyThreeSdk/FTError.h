6699916109da71c68808ea3656ee38f7248e717f
ericroc <eric@fiftythree.com>
2014-07-28 14:46:41 -0400

    Update FTError.h

--- FiftyThreeSdk/FiftyThreeSdk/FTError.h
+++ FiftyThreeSdk/FiftyThreeSdk/FTError.h
@@ -19,7 +19,7 @@ extern NSString * const kFiftyThreeErrorDomain;
 
 enum
 {
-    // The pen connection process fails if upon connecting the pen's tip is not pressed. The user must keep
+    // The pen connection process fails if, upon connecting, the pen's tip is not pressed. The user must keep
     // the pen pressed against the pairing spot until the connection is finished. If he/she releases early,
     // or the connection was established somehow without the tip being pressed, then the connection process
     // should be aborted.
