7edac1b0e2b58c79796b191aef27fe36655de7aa
Peter Sibley <petersibley@gmail.com>
2014-09-26 14:21:36 -0700

    Fixing injector for real sample for injector
    Conflicts:
    	iOS/SDK/FiftyThreeSdk/FiftyThreeSdk/Classification/MissedSampleInjector.hpp

--- FiftyThreeSdk/FiftyThreeSdk/Classification/MissedSampleInjector.hpp
+++ FiftyThreeSdk/FiftyThreeSdk/Classification/MissedSampleInjector.hpp
@@ -7,8 +7,7 @@
 
 #pragma once
 
-#include <boost/make_shared.hpp>
-
+#include "Core/Memory.h"
 #include "FiftyThreeSdk/Classification/DataStream.hpp"
 
 namespace fiftythree
@@ -22,36 +21,35 @@ class MissedSampleInjector
 
 public:
 
-    typedef DataStream<DataType> DataStreamType;
+    using DataStreamType = DataStream<DataType>;
+    using DataStreamTypePtr = typename DataStreamType::Ptr;
+    using Ptr = core::shared_ptr< MissedSampleInjector<DataType>>;
 
-    typename DataStreamType::Ptr _realAndInjectedSamples;
-    typename DataStreamType::Ptr _realSamples;
-    
-    typedef boost::shared_ptr< MissedSampleInjector<DataType> > Ptr;
+    DataStreamTypePtr _realAndInjectedSamples;
+    DataStreamTypePtr _realSamples;
 
     float _minimumInjectedSampleSpacing;
     bool  _useSmoothedSamples;
 
-    static MissedSampleInjector<DataType>::Ptr New() { return Ptr(new MissedSampleInjector<DataType>); }
+    static MissedSampleInjector<DataType>::Ptr New() { return core::make_shared<MissedSampleInjector<DataType>>();}
 
     MissedSampleInjector() :
-    _realAndInjectedSamples(typename DataStreamType::Ptr(new DataStreamType)),
-    _realSamples(typename DataStreamType::Ptr(new DataStreamType)),
+    _realAndInjectedSamples(core::make_shared<DataStreamType>()),
+    _realSamples(core::make_shared<DataStreamType>()),
     _minimumInjectedSampleSpacing(25.0f)
     {
-
     }
 
     DataStreamType const &RealAndInjectedSamples()
     {
         return *_realAndInjectedSamples;
     }
-    
+
     DataStreamType const &RealSamples()
     {
         return *_realSamples;
     }
-    
+
     void SetSamplingRate(float samplesPerSecond)
     {
         _realAndInjectedSamples->_sampleRate = samplesPerSecond;
@@ -69,25 +67,21 @@ public:
         assert(_realAndInjectedSamples);
 
         int missingCount = 0;
-        int sizeOnEntry = _realAndInjectedSamples->Size();
+        auto sizeOnEntry = _realAndInjectedSamples->Size();
 
-        _realSamples->AddPoint(point, timestamp);
-        
         // check if size >= 2 since the second point on iOS is unpredictable and we don't want to stuff a line
         // segment in there
         if (_realAndInjectedSamples->Size() > 1)
         {
-            
-            
-            float samplingInterval = 1.0f / _realAndInjectedSamples->_sampleRate;
+            auto samplingInterval = 1.0f / _realSamples->_sampleRate;
 
-            float dt = timestamp - _realAndInjectedSamples->LastAbsoluteTimestamp();
+            auto dt = timestamp - _realSamples->LastAbsoluteTimestamp();
 
             missingCount = std::max(0, int(roundf(dt / samplingInterval)) - 1);
 
-            float ds = (_realAndInjectedSamples->LastPoint() - point).norm();
-            
-            if(ds < _minimumInjectedSampleSpacing)
+            auto ds = (_realSamples->LastPoint() - point).norm();
+
+            if (ds < _minimumInjectedSampleSpacing)
             {
                 missingCount = 0;
             }
@@ -95,7 +89,7 @@ public:
             {
                 missingCount = std::min(missingCount, int(ds * 2.0f));
             }
-            
+
             // ds > minspacing just ensures we won't detect a lot of missing samples when they stop
             if (missingCount > 0)
             {
@@ -103,73 +97,72 @@ public:
                 // the missing samples will be recovered by evaluating segment
                 // at times = 0, 1, 2, ..., (missingCount - 1)
                 CubicPolynomial<DataType> segment;
-                
-                typename DataStreamType::Ptr samples = _realSamples;
-                if (_useSmoothedSamples)
-                {
-                    // this case here is not used at the moment, but i suppose it could be useful for something
-                    // so i'm leaving it in.
-                    //samples = _target->SmoothedSamplePoints();
-                }
+                const auto & samples = _realSamples;
+
+                auto fromTimestamp = samples->LastAbsoluteTimestamp();
+                constexpr float relativeFromTimestamp = 0.0f;
+                float relativeToTimestamp = timestamp - fromTimestamp;
+                double TnormalizedFactor = 1.0 / (timestamp - fromTimestamp);
 
                 switch (samples->Size())
                 {
                     case 1:
                     {
-                        DataType fromPoint  = samples->LastPoint();
-                        segment             = CubicPolynomial<DataType>::LineWithValuesAtTimes(fromPoint, point, -1.0f, float(missingCount));
+                        const auto & fromPoint  = samples->LastPoint();
+                        segment = CubicPolynomial<DataType>::LineWithValuesAtTimes(fromPoint,
+                                                                                   point,
+                                                                                   TnormalizedFactor * relativeFromTimestamp,
+                                                                                   TnormalizedFactor * relativeToTimestamp);
                         break;
                     }
                     case 2:
                     default:
                     {
+                        const auto & p = samples->ReverseData(1);
+                        const auto & q = samples->LastPoint();
+                        auto olderFromTimestamp = samples->ReverseAbsoluteTimestamp(1);
+                        float olderRelativeFromTimestamp = olderFromTimestamp - fromTimestamp;
 
-                        DataType p = samples->ReverseData(1);
-                        DataType q = samples->LastPoint();
-
-                        segment = CubicPolynomial<DataType>::QuadraticWithValuesAtTimes(p, q, point, -2.0f, -1.0f, float(missingCount));
+                        segment = CubicPolynomial<DataType>::QuadraticWithValuesAtTimes(p,
+                                                                                        q,
+                                                                                        point,
+                                                                                        TnormalizedFactor * olderRelativeFromTimestamp,
+                                                                                        TnormalizedFactor * relativeFromTimestamp,
+                                                                                        TnormalizedFactor * relativeToTimestamp);
 
                         break;
                     }
-                        // there is no "case 3:" because it was too sensitive to noise.  if you move slowly,
-                        // then suddenly go fast, and detect a few dropped samples, the slow samples produce
-                        // a wildly erratic polynomial and the interpolated samples shoot all over the place.
-                        // this still happens with quadratics to a lesser extent and i need to be a little smarter there.
+
+                    // there is no "case 3:" because it was too sensitive to noise.  if you move slowly,
+                    // then suddenly go fast, and detect a few dropped samples, the slow samples produce
+                    // a wildly erratic polynomial and the interpolated samples shoot all over the place.
+                    // this still happens with quadratics to a lesser extent and i need to be a little smarter there.
                 }
 
-                float t = 0.0f;
+                auto dtInject = dt / float(missingCount + 1);
+                double relativeInjectTimestamp = dtInject;
 
-                double dtInject              = dt / float(missingCount + 1);
-                double injectTimestamp       = _realAndInjectedSamples->LastAbsoluteTimestamp() + dtInject;
-
-                for (int j=0; j<missingCount; j++, t++, injectTimestamp += dtInject)
+                for (int j = 0; j < missingCount; ++j, relativeInjectTimestamp += dtInject)
                 {
-                    DataType injectee = segment.ValueAt(t);
-                    
-                    //std::cerr << "\n inject = (" << injectee.x() << ", " << injectee.y() << "), t = " << injectTimestamp;
-
-                    _realAndInjectedSamples->AddPoint(injectee, injectTimestamp);
-                    //_target->AddPoint(injectee, injectTimestamp);
+                    const auto & injectee = segment.ValueAt(TnormalizedFactor * relativeInjectTimestamp);
+                    DebugAssert(std::isfinite(injectee[0]) && std::isfinite(injectee[1]));
+                    auto absoluteInjectTimestamp = samples->LastAbsoluteTimestamp() + relativeInjectTimestamp;
+                    _realAndInjectedSamples->AddPoint(injectee, absoluteInjectTimestamp);
                 }
             }
         }
-        
-        //std::cerr << "\n addpt = (" << point.x() << ", " << point.y() << "), t = " << timestamp << ", Size() = " << _realAndInjectedSamples->Size();
 
+        _realSamples->AddPoint(point, timestamp);
         _realAndInjectedSamples->AddPoint(point, timestamp);
-        
+
         DebugAssert(_realAndInjectedSamples->Size() - sizeOnEntry == 1 + missingCount);
 
-        
         return 1 + missingCount;
-
     }
 
 };
 
-typedef MissedSampleInjector<Vector1f>         MissedSampleInjector1f;
-typedef MissedSampleInjector<Eigen::Vector2f>  MissedSampleInjector2f;
-typedef MissedSampleInjector<Vector7f>         MissedSampleInjector7f;
-
+using MissedSampleInjector1f = MissedSampleInjector<Vector1f>;
+using MissedSampleInjector2f = MissedSampleInjector<Eigen::Vector2f>;
 }
 }
